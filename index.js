80d732368eb3d5fbb2714d44bd9038cd9a223f56
fadumasaidibrahim <fadumasaid2022@gmail.com>
2023-02-09 09:03:52 +0000

    Define the API settings

---
+++ index.js
@@ -0,0 +1,12 @@
+// Define the API settings
+const settings = {
+	"async": true, // Whether the request should be asynchronous or not
+	"crossDomain": true, // Whether the request is cross-domain or not
+	"url": "https://ip-geolocation-ipwhois-io.p.rapidapi.com/json/", // API endpoint
+	"method": "GET", // HTTP request method
+	"headers": { // API headers
+		"X-RapidAPI-Key": "95d8d32b4dmsh981de6c214c9442p1873d3jsn0bc59b3ac168", // API key
+		"X-RapidAPI-Host": "ip-geolocation-ipwhois-io.p.rapidapi.com" // API host
+	}
+};
+
