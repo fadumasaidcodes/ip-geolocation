01ee792510b2aca5225bf88d00db216546d00dd3
fadumasaidibrahim <fadumasaid2022@gmail.com>
2023-02-09 10:02:48 +0000

    refactoring the code by adding two api

--- index.js
+++ index.js
@@ -2,3 +2,14 @@
 const apiKey = "5f81365ae536b7da813d034c891315db";
 const weatherUrl = "https://api.openweathermap.org/data/2.5/weather";
 
+// Define the IP geolocation API settings
+const geolocationSettings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://ip-geolocation-ipwhois-io.p.rapidapi.com/json/",
+    "method": "GET",
+    "headers": {
+        "X-RapidAPI-Key": "95d8d32b4dmsh981de6c214c9442p1873d3jsn0bc59b3ac168",
+        "X-RapidAPI-Host": "ip-geolocation-ipwhois-io.p.rapidapi.com"
+    }
+};
